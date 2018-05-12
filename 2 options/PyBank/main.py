6f6096b95648ecbb3982e0ff35d03c31c2545232
dinhvuong <vpdinh315@gmail.com>
2018-05-12 15:33:55 -0700

    Updating homework

--- 2 options/PyBank/main.py
+++ 2 options/PyBank/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-pathfile = open("raw_data/budget_data_1.csv",'r')
+pathfile = open("raw_data/budget_data_2.csv",'r')
 csvdata = csv.DictReader(pathfile)
 
 totalrev = [] # Create a list to store every single month's revenue
@@ -16,32 +16,34 @@ for data in csvdata:
 
 #Calculate change by months by get next month subtract current month or vice versa
 end=len(totalrev)-1
+listindex = [] #To store previous Index for look up the Date after calculate average change
 while end >0:
-    average.append(totalrev[end]-totalrev[end-1])    
+    average.append(totalrev[end]-totalrev[end-1]) 
+    #create a list to store index   
+    listindex.append(end)
     end=end-1
 
 indmax=0 # To Look up for index of max value(Greatest increase)
 max1 = 0 # To look up max value(Greatest increase)
 min1 = 0 # To look up max value(Greatest decrease)
 indexmin=0 #To Look up for index of max value(Greatest decrease)
-for index1 in range(len(totalrev)-1):
-    if max1 <= totalrev[index1]:
-        max1 = totalrev[index1]
-        indexmax=index1
-    if min1 >=totalrev[index1]:
-        min1=totalrev[index1]
-        indexmin=index1
-    
-#Simple ways by reusing build-in functions: max,min, sum on the list contain Revenue to come out the results        
-#indexmax = totalrev.index(max(totalrev))
-#indexmin = totalrev.index(min(totalrev))
+i=len(average)-1
+#for index1 in range(len(average)-1):
+while i >= 0:
+    if max1 <= average[i]:
+        max1 = average[i]
+        indexmax = listindex[i]
+    if min1 >=average[i]:
+        min1=average[i]
+        indexmin=listindex[i]
+    i=i-1
 
 print("```")
 print("Financial Analysis")
 print("-"*30)
 print("Total Month:" + " " + str(len(totalrev)))
 print("Total Revenue:"+" " + str(totalrevenue))
-print("Average Revenue Change:" + " " + str(sum(average)/len(average)))
+print("Average Revenue Change:" + " " + str(sum(average)/len(totalrev)))
 print("Greatest Increase in Revenue:" + " " + date[indexmax] + " " + "("+"$"+ str(max1)  + ")")
 print("Greatest Decrease in Revenue:" + " " + date[indexmin] + " " + "("+"$"+ str(min1)  + ")")
 print("```")
@@ -52,7 +54,7 @@ file.write("Financial Analysis \n")
 file.write("-"*30+"\n") 
 file.write("Total Month:" + " " + str(len(totalrev))+"\n")
 file.write("Total Revenue:"+" " + str(totalrevenue)+"\n")
-file.write("Average Revenue Change:" + " " + str(sum(average)/len(average))+"\n")
+file.write("Average Revenue Change:" + " " + str(sum(average)/len(totalrev))+"\n")
 file.write("Greatest Increase in Revenue:" + " " + date[indexmax] + " " + "("+"$"+ str(max1)  + ")"+"\n")
 file.write("Greatest Decrease in Revenue:" + " " + date[indexmin] + " " + "("+"$"+ str(min1)  + ")"+"\n")
 file.write ("```\n")
